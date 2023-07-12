6a65fc23b4a6844d3159ec4e4eeb2025307e76b8
Sinem Arslan <sinemarslan1997@gmail.com>
2023-07-12 20:17:33 +0300

    Gun-1

---
+++ gun-1/JS/main.js
@@ -0,0 +1,32 @@
+let firstName = 'Sinem'
+let lastName = 'Arslan'
+let country = 'Turkey'
+let city = 'Ankara'
+let age = 25
+let isMarried = false
+console.log(firstName, lastName, country, city, age, isMarried)
+
+const gravity = 9.81 // earth gravity  in m/s2
+const boilingPoint = 100 // water boiling point, temperature in °C
+const PI = 3.14 // geometrical constant
+console.log(gravity, boilingPoint, PI)
+
+// Variables can also be declaring in one line separated by comma, however I recommend to use a seperate line to make code more readble
+let name = 'Sinem', job = 'Engineer', live = 'Turkey'
+console.log(name, job, live)
+
+let phoneNumber
+let schoolName
+let graduationYear
+let facultyName
+
+let favouriteFood = 'Hamburger'
+let hairColor = 'Ginger'
+let luckyNumber = '7'
+let nickName = 'oliviapingu'
+
+let myAge = '25'
+let yourAge = '26'
+
+console.log('I am', myAge, 'years old.')
+console.log('I am', yourAge, 'years old.')
---
+++ gun-1/JS/variable.js
@@ -0,0 +1,6 @@
+let birthYear = 1997
+let hasCat = false
+let catName
+console.log(catName) //Undefined
+let emptyValue = null
+console.log(emptyValue)
